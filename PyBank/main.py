3817d93d808374269d947bf029d7b6b69d9c9852
su <adsu@uci.edu>
2021-07-02 15:48:45 -0700

    uupdating pybank script

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,18 +18,45 @@ with open (csvpath) as csvfile:
         #net amount of profit/loss
         net = 0
 
+        value = 0
+        change = 0
+        dates = []
+        profits = []
+
         for row in csvreader:
+            #counting up months as rows go by
             months += 1
+            #summing up profits/losses as rows go by
             net += int(row[1])
 
+            #adding dates to a separate list
+            dates.append(row[0])
 
+            #recording change
+            change = int(row[1]) - value
+            value = int(row[1])
 
-        average = net/months
+#Looking at greatest increase in profits
+maximum = max(profits)
+maxindex = profits.index(maximum)
+maxdate = dates[maxindex]
 
-        
-print(months)
-print(net)
-print(average)
+#doing same thing for greatest decrease
+minimum = min(profits)
+minindex = profits.index(minimum)
+mindate = dates[minindex]
+
+#calculating average change
+average = sum(profits)/len(profits)
+
+#Showing results
+print("Financial Analysis")
+print("----------------------------")      
+print(f"Total Months: {str(months)}")
+print(f"Total: ${str(net)}")
+print(f"Average Change: ${str(average)}")
+print(f"Greatest Increase in Profits: {maxdate} ${str(maximum)}")
+print(f"Greatest Decrease in Profits: {mindate} ${str(minimum)}")
 
 
 
