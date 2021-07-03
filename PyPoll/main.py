f4975042e41fc7fa03f345d4ff5d4cd4de79486b
su <adsu@uci.edu>
2021-07-02 17:27:32 -0700

    updating poll script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,7 +26,7 @@ with open(csvpath) as csvfile:
         for row in csvreader:
             total += 1
 
-            #adding candidate in list
+            #adding candidate in list and summing votes per candidate
             if row[2] not in candidates:
                 candidates.append(row[2])
                 index = candidates.index(row[2])
@@ -35,6 +35,12 @@ with open(csvpath) as csvfile:
                 index = candidates.index(row[2])
                 candivotes[index] += 1
         
+        #calculating percentage 
         for vote in candivotes:
             percent = round((vote/total) * 100, 3)
             percentage.append(percent)
+
+        #calculating winner
+        maximum = max(candivotes)
+        index = candivotes.index(maximum)
+        winner = candidates[index]
