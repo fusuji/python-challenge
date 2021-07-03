20f39be8114fde10fdfb0f85c2eb2060fa72b5f4
su <adsu@uci.edu>
2021-07-02 17:16:27 -0700

    updating pypoll script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,4 +8,33 @@ csvpath = os.path.join("PyPoll", "Resources", "election_data.csv")
 #opening csv file
 with open(csvpath) as csvfile:
         csvreader = csv.reader(csvfile, delimiter = ',')    
+
+        #read header first
+        csv_header = next(csvreader)
+
+        #making a list for candidates
+        candidates = []
+
+        #making list for percentage
+        percentage = []
+
+        total = 0
+
+        #making list for each candidate votes
+        candivotes = []
+
+        for row in csvreader:
+            total += 1
+
+            #adding candidate in list
+            if row[2] not in candidates:
+                candidates.append(row[2])
+                index = candidates.index(row[2])
+                candivotes.append(1)
+            else:
+                index = candidates.index(row[2])
+                candivotes[index] += 1
         
+        for vote in candivotes:
+            percent = round((vote/total) * 100, 3)
+            percentage.append(percent)
